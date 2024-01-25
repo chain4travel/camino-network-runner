2e8d7fa8a6999cb221453f6baf36f9fd9a64a806
evlekht <ev.lekht@gmail.com>
2024-01-25 19:16:19 +0400

    build script fix

--- scripts/build.sh
+++ scripts/build.sh
@@ -18,9 +18,8 @@ else
     OUTPUT=$1
 fi
 
-echo "CNR PATH $CAMINO_NETWORK_RUNNER_PATH"
-
-go build -C $CAMINO_NETWORK_RUNNER_PATH -v -ldflags="-X 'github.com/ava-labs/avalanche-network-runner/cmd.Version=$VERSION'" -o $OUTPUT/camino-network-runner 
+cd $CAMINO_NETWORK_RUNNER_PATH
+go build -v -ldflags="-X 'github.com/ava-labs/avalanche-network-runner/cmd.Version=$VERSION'" -o $OUTPUT/camino-network-runner 
 
 # Exit build successfully if the binaries are created
 if [[ -f "$CAMINO_NETWORK_RUNNER_PATH/$OUTPUT/camino-network-runner" ]]; then
